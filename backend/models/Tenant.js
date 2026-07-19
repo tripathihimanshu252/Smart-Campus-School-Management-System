fae0ed4b3e60d1a91e1a4fe30febbd233ce52bcd
Himanshu Tripathi <tumhara-github-email@example.com>
2026-07-19 19:17:55 +0530

    Updated Tenant Schema to save director details

--- backend/models/Tenant.js
+++ backend/models/Tenant.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const tenantSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    tenantCode: { type: String, unique: true, required: true },
-    tenantId: { type: String, required: true },
-    adminEmail: { type: String, required: true },
-    directorDetails: {
-        name: String,
-        email: String,
-        password: String
-    },
-    status: { type: String, default: 'Active' }
+    name: { type: String, required: true },
+    tenantCode: { type: String, unique: true, required: true },
+    tenantId: { type: String, required: true },
+    adminEmail: { type: String, required: true },
+    directorDetails: {
+        name: String,
+        email: String,
+        password: String
+    },
+    status: { type: String, default: 'Active' }
 }, { timestamps: true });
 
 module.exports = mongoose.models.Tenant || mongoose.model('Tenant', tenantSchema);
